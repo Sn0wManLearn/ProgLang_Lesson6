d3774cf12901622278f86a85103a61ac6be89076
Bl1nker <Alex_001@sibmail.com>
2024-09-27 15:07:35 +0700

    Task 4 done. All done

---
+++ Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+using System;
+class MyClass
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+
+        string[] strings = input.Split(' ');
+
+        string result = "";
+
+        for (int i = strings.Length - 1; i >= 0; i--)
+        {
+            result += strings[i] + " ";
+        }
+
+        Console.WriteLine(result);
+    }
+}
