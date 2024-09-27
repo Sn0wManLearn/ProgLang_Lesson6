4998c08c68d1bad74bac0684a5390e065bf3c613
Bl1nker <Alex_001@sibmail.com>
2024-09-27 14:02:11 +0700

    ask 2 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+using System;
+
+class MyClass
+{
+    public static void Main()
+    {
+        string str = "HeLlO WoRlD!";
+
+        Console.WriteLine(str);
+
+        str = str.ToLower();
+
+        Console.WriteLine(str);
+    }
+}
