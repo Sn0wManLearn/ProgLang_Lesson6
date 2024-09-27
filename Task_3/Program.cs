12a471f7d0ae2000def37795a2efca2d750c619e
Bl1nker <Alex_001@sibmail.com>
2024-09-27 15:00:19 +0700

    Task 3 done

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+using System;
+class MyClass
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        input = input.Replace(" ", "");
+        char[] chars = input.ToCharArray();
+        Array.Reverse(chars);
+        string str2 = new string(chars);
+        if (input == str2) Console.WriteLine("Строка палиндром.");
+        else Console.WriteLine("Строка непалиндром.");
+    }
+}
